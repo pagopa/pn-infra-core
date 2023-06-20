ecd3d0f71a36b47639a8ffb8819cf509708c3673
Fabrizio D'Ammassa <fabrizio.dammassa@pagopa.it>
2023-06-20 15:00:30 +0200

    Added api-ext domain (#12)
    * Added api-ext domain
    
    * Updated api domain for legal conservation

--- src/main/99-outputs.tf
+++ src/main/99-outputs.tf
@@ -111,6 +111,10 @@ output "Core_SelcpgApiDnsName" {
   value = "api-selcpg.${var.dns_zone}"
 }
 
+output "Core_CNApiDnsName" {
+  value = "api.cn.${var.dns_zone}"
+}
+
 output "Core_WebApiDnsName" {
   value = "webapi.${var.dns_zone}"
 }
