a4ddcecb49c43c48bfcde3faec3280214728fe4a
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2024-02-15 17:04:45 +0100

    refactor: change variable names for better semantics and readability

--- src/main/70-diagnostic-lambda.tf
+++ src/main/70-diagnostic-lambda.tf
@@ -1,8 +1,9 @@
 locals {
-  confinfo_assumerole_arn  = "arn:aws:iam::${var.pn_confinfo_aws_account_id}:role/DiagnosticAssumeRole"
-  data_proxy_function_name = "diagnostic-data-proxy"
-  data_proxy_region        = var.aws_region
-  pn_data_vault_base_url   = "http://alb.confidential.pn.internal:8080"
+  confinfo_assumerole_arn           = "arn:aws:iam::${var.pn_confinfo_aws_account_id}:role/DiagnosticAssumeRole"
+  confinfo_data_proxy_function_name = "diagnostic-data-proxy"
+  confinfo_data_proxy_region        = var.aws_region
+
+  alb_confidential_base_url = "http://${aws_route53_record.dev-ns}:8080"
 
   diagnostic_tools_function_name = "diagnostic-tools"
   diagnostic_tools_filename      = "${path.root}/../../functions/diagnostic-tools/function.zip"
@@ -18,10 +19,10 @@ module "diagnostic_tools" {
   aws_region                          = var.aws_region
   pn_core_aws_account_id              = var.pn_core_aws_account_id
   runtime                             = local.diagnostic_tools_runtime
-  diagnostic_data_proxy_lambda_region = local.data_proxy_region
+  diagnostic_data_proxy_lambda_region = local.confinfo_data_proxy_region
   diagnostic_assumerole_arn           = local.confinfo_assumerole_arn
-  diagnostic_data_proxy_function_name = local.data_proxy_function_name
-  pn_data_vault_base_url              = local.pn_data_vault_base_url
+  diagnostic_data_proxy_function_name = local.confinfo_data_proxy_function_name
+  alb_confidential_base_url              = local.alb_confidential_base_url
   vpc_subnet_ids                      = module.vpc_pn_core.private_subnets
   vpc_id                              = module.vpc_pn_core.vpc_id
 }
