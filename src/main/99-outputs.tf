9fe82d2762c5a5e9dc4dc6811618110ea09a9844
Fabrizio D'Ammassa <fabrizio.dammassa@pagopa.it>
2024-09-16 11:00:00 +0200

    PN-12354: add api.bg api domain (#41)
    * PN-12354: add api.bg api domain
    
    * PN-12354: fix api dest domain

--- src/main/99-outputs.tf
+++ src/main/99-outputs.tf
@@ -137,7 +137,9 @@ output "Core_BoApiDnsName" {
   value = "api.bo.${var.dns_zone}"
 }
 
-
+output "Core_DestApiDnsName" {
+  value = "api.dest.${var.dns_zone}"
+}
 output "Core_CdnZoneId" {
   value = local.account_root_dns_zone_id
 }
