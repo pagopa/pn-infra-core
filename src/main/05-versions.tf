175f305d9549c3bfb8128759164aa93840ecec14
Alessio Angelotti <alessio.angelotti@pagopa.it>
2025-07-04 17:12:45 +0200

    PN-14969: changed AWS provider to 5.79, updated public vpc module version from 3.19 to 5.17

--- src/main/05-versions.tf
+++ src/main/05-versions.tf
@@ -1,12 +1,12 @@
 terraform {
-  required_version = "1.4.2"
+  required_version = "1.5.7"
 
   backend "s3" {}
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.60.0"
+      version = "5.79.0"
     }
   }
 }
