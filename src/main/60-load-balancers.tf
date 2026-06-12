7ccaa099fc6fc378c36797bc84221b2b41937c2c
Alessio Angelotti <alessio.angelotti@pagopa.it>
2026-06-12 12:53:38 +0200

    feat(core): align Service Desk TG name to dedicated listener

--- src/main/60-load-balancers.tf
+++ src/main/60-load-balancers.tf
@@ -457,21 +457,17 @@ resource "aws_lb_listener" "pn_core_servicedesk_nlb_http_to_alb_http" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.pn_core_servicedeskin_nlb_http_to_alb_http.arn
+    target_group_arn = aws_lb_target_group.pn_core_servicedesk_nlb_http_to_dedicated_alb_listener_http.arn
   }
 }
-# - Service Desk NLB target group for dedicated HTTP listener
-resource "aws_lb_target_group" "pn_core_servicedeskin_nlb_http_to_alb_http" {
-  name_prefix = "SeDeI-"
+# - Service Desk NLB target group for dedicated ALB listener
+resource "aws_lb_target_group" "pn_core_servicedesk_nlb_http_to_dedicated_alb_listener_http" {
+  name_prefix = "SeDeP-"
   vpc_id      = module.vpc_pn_core.vpc_id
 
   port        = var.servicedesk_private_link_listener_port
   protocol    = "TCP"
   target_type = "alb"
-
-  lifecycle {
-    create_before_destroy = true
-  }
 
   depends_on = [
     aws_lb.pn_core_servicedesk_nlb,
@@ -488,15 +484,12 @@ resource "aws_lb_target_group" "pn_core_servicedeskin_nlb_http_to_alb_http" {
     matcher = "200-499"
   }
 }
-resource "aws_lb_target_group_attachment" "pn_core_servicedeskin_nlb_http_to_alb_http" {
-  target_group_arn  = aws_lb_target_group.pn_core_servicedeskin_nlb_http_to_alb_http.arn
+
+resource "aws_lb_target_group_attachment" "pn_core_servicedesk_nlb_http_to_dedicated_alb_listener_http" {
+  target_group_arn  = aws_lb_target_group.pn_core_servicedesk_nlb_http_to_dedicated_alb_listener_http.arn
   port              = var.servicedesk_private_link_listener_port
 
   target_id         = aws_lb.pn_core_ecs_alb.arn
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
 resource "aws_s3_bucket" "pn_core_alb_logs_bucket" {
